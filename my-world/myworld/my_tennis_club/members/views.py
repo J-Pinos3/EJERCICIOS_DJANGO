f3f0442b5276fe82330ffb3a1fe00c99a4a2889d
J-Pinos3 <josloco665@gmail.com>
2023-08-20 17:11:47 -0500

    django admin, new field to the model Member

--- my-world/myworld/my_tennis_club/members/views.py
+++ my-world/myworld/my_tennis_club/members/views.py
@@ -35,3 +35,12 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits':['Apple','Banana','Cherry',],
+    'firstname':'José Pinos'
+  }
+  return HttpResponse(template.render(context, request))
