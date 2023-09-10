d67be2e7c73feea899243de1000a7bcdc8b2dbcb
J-Pinos3 <josloco665@gmail.com>
2023-09-10 15:37:00 -0500

    Register, show users in an table

--- dcrm/dcrm/website/views.py
+++ dcrm/dcrm/website/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+
+    records = Record.objects.all().values()
 
     #check to see if user is logged in, I get what's inside form's login boxes
     if request.method == 'POST':
@@ -22,12 +26,16 @@ def home(request):
             return redirect('home')
             
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
+
+
 
 """
 def login_user(request):
     pass
 """
+
+
 
 def logout_user(request):
     logout(request)
@@ -36,4 +44,26 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authentica and login  ACORDING WITH forms.py
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You\'ve succesfully registered.')
+            return redirect('home')
+    else: 
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'recotd.html', {'customer_record':focustomer_record})
+    else:
+        messages.success(request, 'You must be logged in.')
+        return redirect('home')
