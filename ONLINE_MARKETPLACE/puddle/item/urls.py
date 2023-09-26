3de60c3ed34100243b82065ffa1e1efaed72e3e3
J-Pinos3 <josloco665@gmail.com>
2023-09-25 21:02:56 -0500

    Browse (search) items and filter them

--- ONLINE_MARKETPLACE/puddle/item/urls.py
+++ ONLINE_MARKETPLACE/puddle/item/urls.py
@@ -9,6 +9,7 @@ app_name = 'item'
 urlpatterns = [
     #path('item/detail/<int:pk>', detail, name='detail'),
     path('new/', views.new, name='new'),
+    path('', views.items, name='items'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/edit/', views.edit, name='edit'),
