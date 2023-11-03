29af7f50e4cf37226ea98ffdd146aca55072c7e0
J-Pinos3 <josloco665@gmail.com>
2023-11-03 14:52:54 -0500

    Showing customer's products in cart and in checkout

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -20,9 +20,22 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items=[]
-    return render(request, 'store/cart.html', {'items':items})
+        #if user is not authenticated the code above will throw an error
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    return render(request, 'store/cart.html', {'items':items, 'order':order})
 
 
-def checkout(request):    
-    context = {}
+def checkout(request): 
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        #if can not find an item, create it
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        #items = OrderItem.objects.filter(order=order)
+        # all the order-items that have order as a parent
+        items = order.orderitem_set.all()
+    else:
+        items=[]
+        #if user is not authenticated the code above will throw an error
+        order = {'get_cart_total':0, 'get_cart_items':0} 
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
