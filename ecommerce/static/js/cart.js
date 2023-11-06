25b062fbc3a9642602c3a5a184d3258f637bc48a
J-Pinos3 <josloco665@gmail.com>
2023-11-06 17:51:09 -0500

    Check wether the user is logged in or not within cart.js , When we click add to cart we will send productId and action  as json to  update_item/ also created our own csrftoken

---
+++ ecommerce/static/js/cart.js
@@ -0,0 +1,46 @@
+
+
+var updateBtns = document.getElementsByClassName('update-cart')
+for(let i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        //where productId an action are the atributes I created in store.html
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:',productId, 'Action:', action)
+
+        console.log("USER", user)
+        if( user === 'AnonymousUser' ){
+            console.log("Not Logged in")
+        }else{
+            updateUserOrder(productId, action)
+        }
+
+    })
+}
+
+//this function will do a POST request to api /update_item/
+//then we'll get the item as json format
+//without the csrf token we wont be able to make this request
+//I'll create the token inside main.html
+function updateUserOrder(productId, action){
+    console.log('User is authsenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url,{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X_CSRFToken': csrftoken
+        },
+        body:JSON.stringify({'productId':productId, 'action': action})
+        //body is the data we send to backend
+    })
+    .then((response)=>{
+        return response.json
+    })
+    .then((data)=>{
+        console.log('Data:', data)
+        //location.reload()
+    })
+}
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
 
 
@@ -39,3 +41,11 @@ def checkout(request):
         order = {'get_cart_total':0, 'get_cart_items':0} 
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+    return JsonResponse('Item was added', safe=False)
